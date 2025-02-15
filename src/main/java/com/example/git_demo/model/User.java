0a31331e5484c5c87da3424e8c156b0bedbec8d0
Darshan0370 <dvdarshan37@gmail.com>
2025-02-15 16:08:18 +0530

    Modified User - Added new field email

--- src/main/java/com/example/git_demo/model/User.java
+++ src/main/java/com/example/git_demo/model/User.java
@@ -3,6 +3,7 @@ package com.example.git_demo.model;
 public class User {
     private int id;
     private String name;
+    private String email;
     private String password;
 
     public int getId() {
@@ -27,5 +28,13 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
